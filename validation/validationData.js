2c95a5c222675fab859263db76be5f243b9de3ba
Thom MF <tigredonorte3@gmail.com>
2014-09-02 09:07:18 -0300

    ValidationData

---
+++ validation/validationData.js
@@ -0,0 +1,43 @@
+
+function validDate(campo,valor) {
+	var date=valor;
+	var ardt=new Array;
+	var ExpReg=new RegExp("(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}");
+	ardt=date.split("/");
+	erro=false;
+	if ( date.search(ExpReg)==-1){
+		erro = true;
+		}
+	else if (((ardt[1]==4)||(ardt[1]==6)||(ardt[1]==9)||(ardt[1]==11))&&(ardt[0]>30))
+		erro = true;
+	else if ( ardt[1]==2) {
+		if ((ardt[0]>28)&&((ardt[2]%4)!=0))
+			erro = true;
+		if ((ardt[0]>29)&&((ardt[2]%4)==0))
+			erro = true;
+	}
+	if (erro) {
+                message_json({success:"'" + valor + "' não é uma data válida!!!"}, 3000);
+		campo.focus();
+		campo.value = "";
+		return false;
+	}
+	return true;
+}
+
+function onlyNumber(e){
+    var tecla=(window.event)?event.keyCode:e.which;   
+    if((tecla>47 && tecla<58)) return true;
+    else{
+    	if (tecla==8 || tecla==0) return true;
+	else  return false;
+    }
+}
+
+function onlyNumberComma( obj , e ){
+    var tecla = ( window.event ) ? e.keyCode : e.which;
+    if ( tecla == 8 || tecla == 0 )
+        return true;
+    if ( tecla != 44 && tecla < 48 || tecla > 57 )
+        return false;
+}
